5cae02a1ad00b5413d861d853f1b43c1fe391581
Cong <53280539+Jero327@users.noreply.github.com>
2023-10-30 10:18:44 +1300

    Dogs

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,20 @@
+import Dog from './Dog'
+
 function App() {
   return (
-    <div className="container">
-      <img
-        className="spinner"
-        alt="a spinning paw-print"
-        src="/images/paw.png"
-      />
-    </div>
+    <>
+      <div className="container">
+        <img
+          className="spinner"
+          alt="a spinning paw-print"
+          src="/images/paw.png"
+        />
+        <Dog name="Desdemona" breed="Bulldog" superpower="Heat vision" />
+        <Dog name="Mm" breed="Keri" superpower="Legs" />
+      </div>
+
+      <Dog name="Happy" breed="Golden" superpower="Buiding" />
+    </>
   )
 }
 
