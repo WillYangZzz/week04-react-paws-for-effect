277c55d4d50cc73ed2e82144da9b5ef17b7ccbf6
David <14233215+davidlpoole@users.noreply.github.com>
2023-10-30 10:32:34 +1300

    finished the challenge

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,3 +1,6 @@
+import Dog from './Dog'
+import Subtitle from './Subtitle'
+
 function App() {
   return (
     <div className="container">
@@ -5,6 +8,25 @@ function App() {
         className="spinner"
         alt="a spinning paw-print"
         src="/images/paw.png"
+      />
+      <Subtitle text="Canines using super-canine abilities for social good." />
+      <Dog
+        name="Desdemona"
+        breed="dachshund"
+        superpower="Heat vision"
+        image="/public/images/dachshund.png"
+      />
+      <Dog
+        name={'Jonas'}
+        breed={'Bulldog'}
+        superpower={'sense of smell'}
+        image="/public/images/bulldog.png"
+      />
+      <Dog
+        name={'Vitor'}
+        breed={'Dogue Brasileiro'}
+        superpower={'trapper'}
+        image="/public/images/hound.png"
       />
     </div>
   )
---
+++ client/components/Dog.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  name: string
+  breed: string
+  superpower: string
+  image: string
+}
+
+function Dog(props: Props) {
+  return (
+    <div className="dog-wrapper">
+      <img src={props.image} alt="DAWG" />
+      <div className="dog">
+        <div className="dog-name-plate">
+          <span className="dog-name">{props.name}</span>
+          <span className="dog-breed">{props.breed}</span>
+        </div>
+        <span className="dog-superpower">{props.superpower}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Dog
