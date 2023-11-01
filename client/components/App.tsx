c27fcd10717754d86afc34d4d80d40e3fdc79c11
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-11-01 23:27:21 +1300

    Create dogs

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,3 +1,5 @@
+import Dog from './Dog'
+
 function App() {
   return (
     <div className="container">
@@ -6,6 +8,11 @@ function App() {
         alt="a spinning paw-print"
         src="/images/paw.png"
       />
+      <Dog name="Tarzan" breed="Chihuahua" superpower="Teleportation" />
+      <Dog name="Lazarus" breed="Chihuahua" superpower="Revival" />
+      <Dog name="Mathilda" breed="Chihuahua" superpower="Telekinesis" />
+      <Dog name="Imelda" breed="Chihuahua" superpower="Shoemaking" />
+      <Dog name="Alice" breed="Chihuahua" superpower="World building" />
     </div>
   )
 }
