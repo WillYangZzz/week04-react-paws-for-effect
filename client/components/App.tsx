3d2179a222450892f89c10bc56d35c02cc7418d1
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-11-01 23:44:09 +1300

    Add pics for dogs

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import Dog from './Dog'
+import Subtitle from './Subtitle'
 
 function App() {
   return (
@@ -8,11 +9,38 @@ function App() {
         alt="a spinning paw-print"
         src="/images/paw.png"
       />
-      <Dog name="Tarzan" breed="Chihuahua" superpower="Teleportation" />
-      <Dog name="Lazarus" breed="Chihuahua" superpower="Revival" />
-      <Dog name="Mathilda" breed="Chihuahua" superpower="Telekinesis" />
-      <Dog name="Imelda" breed="Chihuahua" superpower="Shoemaking" />
-      <Dog name="Alice" breed="Chihuahua" superpower="World building" />
+      <Dog
+        name="Tarzan"
+        breed="Chihuahua"
+        superpower="Teleportation"
+        image="/images/scottie.png"
+      />
+      <Subtitle text="Loves to swing around the trees and teleport if he falls." />
+      <Dog
+        name="Lazarus"
+        breed="Chihuahua"
+        superpower="Revival"
+        image="/images/scottie.png"
+      />
+      <Subtitle text="Frequently comes back from the dead." />
+      <Dog
+        name="Mathilda"
+        breed="Chihuahua"
+        superpower="Telekinesis"
+        image="/images/scottie.png"
+      />
+      <Dog
+        name="Imelda"
+        breed="Chihuahua"
+        superpower="Shoemaking"
+        image="/images/scottie.png"
+      />
+      <Dog
+        name="Alice"
+        breed="Chihuahua"
+        superpower="World building"
+        image="/images/scottie.png"
+      />
     </div>
   )
 }
--- client/components/Dog.tsx
+++ client/components/Dog.tsx
@@ -2,6 +2,7 @@ interface Props {
   name: string
   breed: string
   superpower: string
+  image: string
 }
 
 function Dog(props: Props) {
@@ -12,6 +13,7 @@ function Dog(props: Props) {
           <span className="dog-name">{props.name}</span>
           <span className="dog-breed">{props.breed}</span>
         </div>
+        <img src={props.image} alt="A chihuahua dressed as a scottie" />
         <span className="dog-superpower">{props.superpower}</span>
       </div>
     </div>
